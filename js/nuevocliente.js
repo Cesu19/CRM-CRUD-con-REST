1f00b6d86ef1bacac5b9f772a568a4f06b9b85c6
cesu19 <cesarhernancorbalan19@gmail.com>
2023-11-30 23:03:05 -0300

    Cree la validación del formulario para agregar un nuevo cliente

---
+++ js/nuevocliente.js
@@ -0,0 +1,35 @@
+( function() {
+
+    const formulario = document.querySelector('#formulario');
+    formulario.addEventListener('submit', validarCliente);
+
+    function validarCliente(e) {
+        e.preventDefault();
+
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+
+        if( validar(cliente)){
+            //Mostrar mensaje
+            console.log('Todos los campos son onbligatorios');
+            return;
+        }
+
+        console.log('Se paso la validacion correctamente');
+    }
+    // Función de validación
+    function validar(obj) {
+        return !Object.values(obj).every( input => input !== '')
+    }
+
+
+})();
